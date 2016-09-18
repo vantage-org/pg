a4fef9b4525acc3329e5b819efcc098e2ec61324
William Mayor <git@williammayor.co.uk>
2016-09-18 15:58:50 +0200

    Replace label system with IP checking

--- tests/test_new.sh
+++ tests/test_new.sh
@@ -6,9 +6,9 @@ set -e
 env_file=$(mktemp)
 vg __env DATABASE_URL "foo_bar"
 
-count_before=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_before=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 url=$(vg -e "$env_file" pg new)
-count_after=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_after=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 
 # Should create one pg DB (with the right label)
 assert_raises "test $count_after -eq $(( $count_before + 1 ))"
@@ -16,9 +16,8 @@ assert_raises "test $count_after -eq $(( $count_before + 1 ))"
 # Should set the database url in the env file
 assert_raises "test $url == $(vg -e "$env_file" __env DATABASE_URL)"
 
-# Should set the DB name as a label
-db_name=${url##*/}
-container_id=$(docker ps --quiet --filter "label=vantage.pg.db_name=$db_name")
+# Should set the vantage.pg label
+container_id=$(docker ps --quiet --filter "label=vantage.pg=1")
 assert_raises "test -n $container_id"
 assert_raises "test 1 -eq $(echo $container_id | wc -l)"
 
--- tests/test_rm.sh
+++ tests/test_rm.sh
@@ -5,33 +5,33 @@ set -e
 
 # Remove using var
 url=$(vg pg new)
-count_before=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_before=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 assert_raises "vg -v DATABASE_URL=$url pg rm"
-count_after=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_after=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 assert_raises "test $count_after -eq $(( $count_before - 1 ))"
 
 # Remove using env file
 env_file=$(mktemp)
 url=$(vg -e "$env_file" pg new)
-count_before=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_before=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 assert_raises "vg -e $env_file pg rm"
-count_after=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count_after=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
+assert_raises "test $count_after -eq $(( $count_before - 1 ))"
 
 # Remove all
 vg pg rm -a > /dev/null
 vg pg new > /dev/null
 vg pg new > /dev/null
-count=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 assert_raises "test $count -eq 2"
 vg pg rm -a > /dev/null
-count=$(docker ps --filter "label=vantage.pg.db_name" | grep postgres | wc -l)
+count=$(docker ps --filter "label=vantage.pg" | grep postgres | wc -l)
 assert_raises "test $count -eq 0"
 
-# Should remove the container with the right DB name
+# Should remove the right container
 url=$(vg pg new)
-db_name=${url##*/}
 vg -v DATABASE_URL=$url pg rm > /dev/null
-container_id=$(docker ps --quiet --filter "label=vantage.pg.db_name=$db_name")
+container_id=$(docker ps --quiet --filter "label=vantage.pg")
 assert_raises "test -z $container_id"
 
 vg pg rm -a > /dev/null
